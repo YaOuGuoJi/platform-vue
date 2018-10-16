5686c70928e65740379c29c023b56a231a1998ee
YanRuis <1131411057@qq.com>
2018-10-16 18:01:24 +0800

    销售额与销售量排名

--- src/components/AreaShopRank.vue
+++ src/components/AreaShopRank.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
    <div>
-     <form>
-       <input type="text" v-model="areaId" placeholder="请输入区域id"/>
-       <input type="date" v-model="start" placeholder="请输入开始时间"/>
-       <input type="date" v-model="end" placeholder="请输入结束时间"/>
-       <input type="text" v-model="limit" placeholder="请输入限制"/>
-       <button type="submit" v-on:click="search()">查询</button>
-     </form>
+     <button type="submit" v-on:click="search()">查询</button>
+     <!--<form method="get">-->
+       <!--<input type="text" v-model="areaId" placeholder="请输入区域id"/>-->
+       <!--<input type="date" v-model="start" placeholder="请输入开始时间"/>-->
+       <!--<input type="date" v-model="end" placeholder="请输入结束时间"/>-->
+       <!--<input type="text" v-model="limit" placeholder="请输入限制"/>-->
+       <!--<button type="submit" v-on:click="search()">查询</button>-->
+     <!--</form>-->
    </div>
-   <div id="chart">111</div>
+   <div id="countchart"></div>
+    <div id="pricechart"></div>
  </div>
 </template>
 <script type="text/javascript">
 import axios from 'axios'
+import echarts from 'echarts'
 export default {
   name: 'AreaShopRank',
-  Date () {
+  data () {
     return {
       areaId: 1,
       start: '2018-09-01 00:00:00',
@@ -26,7 +29,7 @@ export default {
   },
   methods: {
     search: function () {
-      axios.get('api/area/shop/rank', {
+      axios.get('/api/area/shop/rank', {
         params: {
           areaId: this.areaId,
           start: this.start,
@@ -42,30 +45,199 @@ export default {
     },
     dataInvoker: function (response) {
       if (!response.success || response.code !== 200){
-        window.alert(response.date)
+        window.alert(response.data);
         return
       }
-      const count = response.date.map(function (count) {
-        return count.number
-      })
-      const shopName = response.date.map(function (shopName) {
-        return shopName.dtoObject.shopName
-      })
-      const shopInfo = response.date.map(function (shopInfo) {
-        return shopInfo.objectMapDto
-      })
-      let chart = echarts.init(document.getElementById('chart'))
-      chart.setOption({
-        title: '区域内前几名商铺排名',
-        xAxis: {},
-        yAxis: {type: 'category'},
-        series: {
-          type: 'bar',
-          name: '商铺信息',
-          data: shopInfo,
-          x: count,
-          y: shopName
-        }
+      console.log(response.data);
+      const count = response.data.orderCount.map(function (shop) {
+        return shop.number;
+      });
+      const countShopName = response.data.orderCount.map(function (shopRank) {
+        return shopRank.dtoObject.shopName
+      });
+      const shopInfo = response.data.orderCount.map(function (shopRank) {
+        return shopRank.dtoObject
+      });
+      let countchart = echarts.init(document.getElementById('countchart'));
+      countchart.setOption({
+        title: {
+          text: '区域内商铺销售量前几名排名',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          textStyle:{
+            color:'#fff',
+          },
+        },
+        grid: {//设置图表位置
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          color:'#fff',
+          splitLine : {//去掉网格线
+            show : false
+          },
+          axisLine : {
+            lineStyle : {
+              color : '#FFF'
+            },
+          },
+        },
+        yAxis: {
+
+          type: 'category',//轴的类型分两种 1.category（类别）2.value(值)
+          data: countShopName,
+          axisLabel : {
+            show : true,
+            textStyle : {
+              color : '#110022',
+              align : 'right',
+              fontSize: 15    /*文字大小*/
+            }
+          },
+          axisLine : {
+            lineStyle : {
+              color : '#110022'//轴的颜色
+            },
+            onZero: false
+          },
+          axisTick : {
+            lineStyle : {
+              color : '#110022'//轴上点的颜色
+            }
+          },
+          inverse: true
+        },
+        series: [
+          {
+            type: 'bar',
+            data: count,
+            label : {
+              normal : {
+                show : true,//显示数字
+                position : 'right'
+              }
+            },
+
+            barWidth : 40,//柱子宽度
+            itemStyle : {
+              normal : {
+                color: '#F08080',//柱状的颜色
+                label : {
+                  textStyle : {
+                    fontSize : '15',//柱状上的显示的文字
+                    color:'#110022'
+                  }
+                }
+              }
+            },
+
+          }
+
+        ]
+      });
+      const price = response.data.orderPrice.map(function (shop) {
+        return shop.number;
+      });
+      const priceShopName = response.data.orderPrice.map(function (shopRank) {
+        return shopRank.dtoObject.shopName
+      });
+      let pricechart = echarts.init(document.getElementById('pricechart'));
+      pricechart.setOption({
+        title: {
+          text: '区域内商铺销售额前几名排名',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          textStyle:{
+            color:'#fff',
+          },
+        },
+        grid: {//设置图表位置
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          color:'#fff',
+          splitLine : {//去掉网格线
+            show : false
+          },
+          axisLine : {
+            lineStyle : {
+              color : '#FFF'
+            },
+          },
+        },
+        yAxis: {
+
+          type: 'category',//轴的类型分两种 1.category（类别）2.value(值)
+          data: priceShopName,
+          axisLabel : {
+            show : true,
+            textStyle : {
+              color : '#110022',
+              align : 'right',
+              fontSize: 15    /*文字大小*/
+            }
+          },
+          axisLine : {
+            lineStyle : {
+              color : '#110022'//轴的颜色
+            },
+            onZero: false
+          },
+          axisTick : {
+            lineStyle : {
+              color : '#110022'//轴上点的颜色
+            }
+          },
+          inverse: true
+        },
+        series: [
+          {
+            type: 'bar',
+            data: price,
+            label : {
+              normal : {
+                show : true,//显示数字
+                position : 'right'
+              }
+            },
+
+            barWidth : 40,//柱子宽度
+            itemStyle : {
+              normal : {
+                color:'#9f9ff2',//柱状的颜色
+                label : {
+                  textStyle : {
+                    fontSize : '15',//柱状上的显示的文字
+                    color:'#110022'
+                  }
+                }
+              }
+            },
+
+          }
+
+        ]
+
       })
 
     }
@@ -73,9 +245,14 @@ export default {
 }
 </script>
 <style>
-  #chart{
+  #countchart{
     text-align: center;
-    background-color: darkseagreen;
+    width: 700px;
+    height: 600px;
+    margin-left: 600px;
+  }
+  #pricechart{
+    text-align: center;
     width: 700px;
     height: 600px;
     margin-left: 600px;

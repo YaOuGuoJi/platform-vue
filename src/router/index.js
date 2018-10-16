338255da322a8e2ecdd0dac1b0d12f7134072b97
zh <277708272@qq.com>
2018-10-16 18:34:05 +0800

    save code

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
 import AreaPeople from '@/components/AreaPeople'
 import UserOrder from '@/components/UserOrder'
-
+import UserReport from '@/component/UserReport'
 Vue.use(Router)
 
 export default new Router({
@@ -23,6 +23,11 @@ export default new Router({
       path: '/user/order',
       name: 'UserOrder',
       component: UserOrder
+    },
+    {
+      path: '/user/report',
+      name: 'UserReport',
+      component: UserReport
     }
   ]
 })

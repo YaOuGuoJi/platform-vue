39ce12fb27ace051a745d4189e4fe10137992c29
zh <277708272@qq.com>
2018-10-19 10:52:03 +0800

    调整页面

--- src/components/UserReport.vue
+++ src/components/UserReport.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="all">
     <template>
       <label>请输入用户ID</label>
       <input v-model="userId" placeholder="例如100000000"/>
@@ -14,31 +14,30 @@
         <h1>{{userReportData.userInfo.userName}}的用户报告</h1>
       </div>
       <div class="survey">
-        <p>
-          {{year}}年{{month === null ? month : month + '月'}}您在亚欧国际小镇共下单{{userReportData.orderNumber}}次,年度消费{{userReportData.totalPrice}}元，超过了{{userReportData.beyondPercent}}%的人，
+        <p id="summary">
+          {{year}}年{{month == null ? month : month + '月'}}您在亚欧国际小镇共下单{{userReportData.orderNumber}}次,年度消费{{userReportData.totalPrice}}元，超过了{{userReportData.beyondPercent}}%的人，
           排名第{{rank}}
         </p>
       </div>
       <div v-if="userReportData.firstOrder">
-        <p>
-        {{userReportData.firstOrder.day}}是您本年度第一次在亚欧国际小镇下单，在{{userReportData.firstOrder.shopName}}购买了{{userReportData.firstOrder.productList}},
-        花费{{userReportData.firstOrder.orderPrice}}元，感谢您的光临
+        <p id="firstOrder">
+        {{userReportData.firstOrder.day}}是您本年度第一次在亚欧国际小镇下单，在{{userReportData.firstOrder.shopName}}购买了{{userReportData.firstOrder.productList}},花费{{userReportData.firstOrder.orderPrice}}元
         </p>
       </div>
       <div class="max_price_order">
-        <p>
+        <p id="maxPrice">
           {{year}}年{{month === null ? month : month + '月'}}消费最高订单：{{userReportData.maxPriceOrder.day}}您在{{userReportData.maxPriceOrder.shopName}}购买了{{userReportData.maxPriceOrder.productList}},
-          花费{{userReportData.maxPriceOrder.orderPrice}}
+          花费{{userReportData.maxPriceOrder.orderPrice}}元
         </p>
       </div>
     </template>
-    <div v-show="showMonthReport">
+    <div id="month-times-and-price" v-show="showMonthReport">
       <div id="month-times" :style="{ width: '600px', height: '500px'}">
       </div>
       <div id="month-price" :style="{ width: '600px', height: '500px'}">
       </div>
     </div>
-    <div v-show="showDayReport">
+    <div id="day-times-and-price" v-show="showDayReport">
       <div id="day-times" :style="{ width: '600px', height: '500px'}">
       </div>
       <div id="day-price" :style="{ width: '600px', height: '500px'}">
@@ -51,23 +50,34 @@
       </div>
     </div>
   </div>
-  <!--<div v-show="showNoRecordPage">-->
-    <!--<div>-->
-      <!--<label>请输入用户ID</label>-->
-      <!--<input v-model="userId" placeholder="例如100000000"/>-->
-      <!--<label>请输入要查询的年份</label>-->
-      <!--<input v-model="year" placeholder="例如2018"/>-->
-      <!--<label>请输入月份（或者不输入）</label>-->
-      <!--<input v-model="month" placeholder="例如9">-->
-      <!--<button v-on:click="search()">点击查询</button>-->
-    <!--</div>-->
-    <!--<p>-->
-      <!--抱歉，未发现您在亚欧国际小镇的订单记录，无法生成用户报告-->
-    <!--</p>-->
-  <!--</div>-->
 </template>
 
 <style scoped>
+
+  #all {
+    width: 1500px;
+  }
+  #month-times-and-price {
+    margin-left: 170px;
+    text-align: center;
+  }
+  #day-times-and-price {
+    text-align: center;
+    margin-left: 170px;
+  }
+  #summary {
+    font-size: xx-large;
+  }
+  #firstOrder {
+    font-size: xx-large;
+    text-indent: 50px;
+  }
+  #maxPrice {
+    font-size: xx-large;
+  }
+  #pay-type {
+    margin-left: 170px;
+  }
   #pay-price {
     float: left;
   }
@@ -113,14 +123,10 @@ export default {
       payPriceOrTimes: [],
       showMonthReport: null,
       showDayReport: null
-      // ,
-      // showRecordPage: true,
-      // showNoRecordPage: false
     }
   },
   methods: {
     search: function () {
-      // console.log('showRecordPage-first-in: ' + this.showRecordPage)
       axios.get('/api/order/user/report', {
         params: {
           userId: this.userId,
@@ -131,30 +137,21 @@ export default {
         if (response.status !== 200 || !response.data) {
           window.alert('请求失败')
         }
-        // console.log('search-in')
-        // console.log(response.data)
         this.dataInvoker(response.data)
       })
     },
     dataInvoker: function (response) {
-      // console.log('dataInvoker-in')
-      // console.log(response)
+      if (!response.success && response.code == 404) {
+        window.alert('未发现用户订单记录，无法生成用户报告')
+        return
+      }
       if (!response.success || response.code !== 200) {
         window.alert(response.message)
         return
       }
       this.userInfo = response.userInfo
       this.userReportData = response.data
-      // console.log(this.userReportData)
       this.orderNumber = response.data.orderNumber
-      // if (this.userReportData != null) {
-      //   this.showRecordPage = true
-      //   this.showNoRecordPage = false
-      // } else {
-      //   this.showRecordPage = false
-      //   this.showNoRecordPage = true
-      // }
-      // console.log('showRecordPageAfterSet: ' + this.showRecordPage)
       this.payType = response.data.payType
       this.totalPrice = response.data.totalPrice
       this.monthReport = response.data.monthReport

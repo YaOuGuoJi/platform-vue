e1722566311716e9f5dc25698397de9fcf7a4781
zh <277708272@qq.com>
2018-12-14 09:28:43 +0800

    修改首页自适应

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -31,7 +31,7 @@
 <style scoped>
   .all {
     width: 100%;
-    height: 938px;
+    height: 100%;
     background: url("../assets/loginBackground.jpg") no-repeat;
     background-size: 100% 100%;
     margin: 0 auto;
